07068c64e427b2e2b5a913a8fef43714694e8683
Mugisha8 <yvesmugisha8@gmail.com>
2024-01-21 23:48:53 +0200

    validation

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,3 +1,18 @@
 import Users from "../models/UserModal.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
+
+//signUP
+
+export const signUp = async (req, res) => {
+  try {
+    // validate Input fields
+
+    const { firstname, lastname, email, password } = req.body;
+
+    if (!firstname || !lastname || !email || !password) {
+      res.status(400).json({
+        status: "400",
+        message: "Missing field",
+      });
+    }
