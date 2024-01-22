295f47e3e5c2bc0376d75623659fe6effb7129c3
Mugisha8 <yvesmugisha8@gmail.com>
2024-01-22 19:25:50 -0300

    using authorization headers

--- middleware/AuthorizationAdmin.js
+++ middleware/AuthorizationAdmin.js
@@ -1,3 +1,14 @@
 import Jwt from "jsonwebtoken";
 
 import Users from "../models/UserModal";
+
+export const AuthorizeAdmin = async (req, res) => {
+    let token;
+  
+    try {
+      if (
+        req.headers.authorization &&
+        req.headers.authorization.startswith("Bearer ")
+      ) {
+        token = req.headers.authorization.split(" ")[1];
+      }
