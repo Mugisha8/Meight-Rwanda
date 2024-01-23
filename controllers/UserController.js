0380bb52f9edce4f3456767b31f90a3b5f379df3
Mugisha8 <yvesmugisha8@gmail.com>
2024-01-23 23:27:41 +0200

    destructuring

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,3 +1,11 @@
 import Users from "../models/UserModal.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
+
+//signUP
+
+export const signUp = async (req, res) => {
+  try {
+    // validate Input fields
+
+    const { firstname, lastname, email, password } = req.body;
